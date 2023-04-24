9b4ac74828be5ad2e99b8bed7eabefb442bbf5a1
funny-ppt <1763341376@qq.com>
2023-04-25 02:11:13 +0800

    0.5改动部分内容：全新的自定义Dialog，在部分位置取代MessageBox和UWP Notification

---
+++ Models/CustomDialogViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaveSyncApp;
+
+public class CustomDialogViewModel : INotifyPropertyChanged
+{
+    private string _title = "";
+    private string _content = "";
+    private readonly Dictionary<string, string> _arguments = new();
+
+    public string Title
+    {
+        get => _title;
+        set
+        {
+            _title = value;
+            OnPropertyChanged(nameof(Title));
+        }
+    }
+
+    public string Content
+    {
+        get => _content;
+        set
+        {
+            _content = value;
+            OnPropertyChanged(nameof(Content));
+        }
+    }
+
+    public void Add(string key, string value)
+    {
+        _arguments.Add(key, value);
+    }
+
+    public string this[string key] { get => _arguments[key]; set => _arguments[key] = value; }
+
+    public IReadOnlyDictionary<string, string> Arguments => _arguments;
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
---
+++ Views/CustomDialog.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SaveSyncApp
+{
+    /// <summary>
+    /// CustomDialog.xaml 的交互逻辑
+    /// </summary>
+    public partial class CustomDialog : Window
+    {
+        public CustomDialogViewModel ViewModel => (CustomDialogViewModel)DataContext;
+
+        public CustomDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void Initialize(string title, string content, (string buttonText, string buttonValue)[] buttons, (string key, string value)[] args)
+        {
+            ViewModel.Title = title;
+            ViewModel.Content = content;
+
+            foreach (var (key, value) in args)
+            {
+                ViewModel.Add(key, value);
+            }
+
+            foreach (var (buttonText, buttonValue) in buttons)
+            {
+                var button = new Button { Content = buttonText, Margin = new Thickness(0, 0, 10, 0), Width = 60 };
+                button.Click += (sender, e) => {
+                    ViewModel["action"] = buttonValue;
+                    DialogResult = true;
+                    Close();
+                };
+                ButtonPanel.Children.Add(button);
+            }
+
+            //ContentTextBlock.Measure(new Size(double.PositiveInfinity, double.PositiveInfinity));
+            //var desiredSize = ContentTextBlock.DesiredSize;
+            //Width = desiredSize.Width;
+        }
+
+        public static IReadOnlyDictionary<string, string>? ShowDialog(string title, string content, (string buttonText, string buttonValue)[] buttons, (string key, string value)[]? args = null)
+        {
+            var customDialog = new CustomDialog();
+            customDialog.Initialize(title, content, buttons, args ?? Array.Empty<(string, string)>());
+
+            // 如果当前上下文是窗口，请使用以下代码将对话框的Owner设置为当前窗口
+            // customDialog.Owner = Application.Current.MainWindow;
+
+            bool? dialogResult = customDialog.ShowDialog();
+
+            if (dialogResult.HasValue && dialogResult.Value)
+            {
+                return customDialog.ViewModel.Arguments;
+            }
+
+            return null;
+        }
+    }
+}
